8329b7a90fd7108216070da19e2195fa5a5ae934
perfeliclemente <perfeliclemente@gmail.com>
2016-03-12 10:42:28 +0800

    countSubstrPattern() done.

--- clemente_ab3l_pa02.h
+++ clemente_ab3l_pa02.h
@@ -1,4 +1,6 @@
 int getHammingDistance(char *str1, char *str2);
+int countSubstrPattern(char *original, char *pattern);
+
 
 int getHammingDistance(char *str1, char *str2){							// This function gets the hamming distance of 2 strings.
 	int i = 0;
@@ -31,6 +33,55 @@ int getHammingDistance(char *str1, char *str2){							// This function gets the 
 		if (str2Len <= 0){
 			printf("Str2 is invalid!\n");
 			return -1;
+		}
+	}	
+}
+
+
+int countSubstrPattern(char *original, char *pattern){						
+	int i = 0;
+	int j = 0;
+	int flag = 0;
+	int acc = 0;
+	int originalLen = strlen(original);
+	int patternLen = strlen(pattern);
+	
+	if ((originalLen > 0) && (patternLen > 0) && originalLen >= patternLen){	
+		for(i = 0; i < originalLen; i++){								
+			
+			for (j = 0;j < patternLen; j++){							
+			
+				if (original[i+j] != pattern[j]){
+					flag = 1;
+				}	
+			
+			}
+			
+			if (flag == 0){										
+				acc++;
+			}else {
+				flag = 0;
+			}
+		}
+		
+		printf("Count: %i\n", acc);
+		return acc;
+			
+	}else{														
+		
+		if (originalLen <= 0){
+			printf("Original str is invalid!\n");
+			return -1;
+		}
+		
+		if (patternLen <= 0){
+			printf("Pattern str is invalid!\n");
+			return -1;
+		}
+		
+		if (originalLen < patternLen){
+			printf("0");
+			return 0;
 		}
 	}	
 }
