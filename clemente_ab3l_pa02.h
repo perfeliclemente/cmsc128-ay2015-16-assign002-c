2b6becf3cccd6ee6b1bc8c92f175c4daa333e2d3
perfeliclemente <perfeliclemente@gmail.com>
2016-03-12 10:38:18 +0800

    getHammingDistance() documentation updated.

--- clemente_ab3l_pa02.h
+++ clemente_ab3l_pa02.h
@@ -1,23 +1,23 @@
 int getHammingDistance(char *str1, char *str2);
 
-int getHammingDistance(char *str1, char *str2){							
+int getHammingDistance(char *str1, char *str2){							// This function gets the hamming distance of 2 strings.
 	int i = 0;
 	int acc = 0;
 	int str1Len = strlen(str1);
 	int str2Len = strlen(str2);
 	
-	if ((str1Len == str2Len) && (str1Len > 0) && (str2Len > 0)){			
+	if ((str1Len == str2Len) && (str1Len > 0) && (str2Len > 0)){			// This checks if the inputs are in a valid form.
 		
-		for (i = 0; i < str1Len; i++){								
+		for (i = 0; i < str1Len; i++){								// This is a loop for checking if there are inversions in every character in the two string.
 			if (str1[i] != str2[i]){
 				acc++;
 			}		
 		}
 		
-		printf("Hamming Distance: %i\n", acc);											
+		printf("Hamming Distance: %i\n", acc);											// This returns the number of inversions between two strings.
 		return acc;	
 		
-	}else{														
+	}else{														// This consists of the conditions not met for the function.
 		if (str1Len != str2Len){
 			printf("Strings are not equal!\n");
 			return -1;
